e89693fdb9476a288f4de831b4050606f0a40dd9
h4de5 <h4de5@users.noreply.github.com>
2021-12-22 00:21:18 +0100

    Implement better switch handling. Add tweens via code. Clean up debug messages. Add variable documentation.

--- godot/Raster.gd
+++ godot/Raster.gd
@@ -2,11 +2,18 @@ extends Node2D
 
 const RasterItem = preload('RasterItem.tscn')
 
+# size of one item
 const rasterSize = 70
+# maxium colums in the grid
 const rasterCols = 10
+# maxium rows in the grid
 const rasterRows = 10
+# time in seconds that a switch will take
+const switchingTime = 0.75
 
+# currently dragged item
 var draggingItem : RasterItem
+# when true, it is not allowed to trigger another switch
 var isSwitching = false
 
 # Called when the node enters the scene tree for the first time.
@@ -20,23 +27,35 @@ func _ready():
 			ItemNode.position.y = y * rasterSize - rasterRows/2 * rasterSize
 			ItemNode.rasterX = x
 			ItemNode.rasterY = y
+			ItemNode.itemId = randi() % 5 - 1
 			ItemNode.name = "Item "+ str(x) + "-" + str(y)
 		
 			ItemNode.connect("StartDragging", self, "startDragging")
 			ItemNode.connect("EndDragging", self, "endDragging")
 			add_child(ItemNode)
 	
+# regenerates the whole raster from scratch
+func generateRaster():
+	pass
+	
+# checks if there are any items that can be marked for deletion
+func checkCombos():
+	pass
 
+# removes all items that are marked for deletion
+func cleanRaster():
+	pass
+
+# starts dragging an item
 func startDragging(startItem: RasterItem):
-	if draggingItem == null && not isSwitching:
+	if draggingItem == null && not isSwitching and not startItem.isSwitching:
 		draggingItem = startItem
 		draggingItem.startSelection()
 		draggingItem.isDragging = true
-	
+
+# let an item drop
 func endDragging(targetItem: RasterItem):
-	
-	if draggingItem != null && not isSwitching:
-		
+	if draggingItem != null && not isSwitching and not targetItem.isSwitching:
 		# only switch if x +- 1 OR y +-1
 		if ((targetItem.rasterX + 1 == draggingItem.rasterX and
 			targetItem.rasterY == draggingItem.rasterY) or
@@ -48,6 +67,8 @@ func endDragging(targetItem: RasterItem):
 			targetItem.rasterY - 1 == draggingItem.rasterY)):
 		
 			isSwitching = true
+			targetItem.isSwitching = true
+			draggingItem.isSwitching = true
 			
 			var oldX = targetItem.rasterX
 			var oldY = targetItem.rasterY
@@ -58,19 +79,8 @@ func endDragging(targetItem: RasterItem):
 			draggingItem.rasterX = oldX
 			draggingItem.rasterY = oldY
 			
-			var tween1 = get_node("Tween1")
-			var tween2 = get_node("Tween2")
-			
-			tween1.interpolate_property(draggingItem, "position",
-					draggingItem.position, targetItem.position, 0.75,
-					Tween.TRANS_BOUNCE, Tween.EASE_OUT)
-			tween1.start()
-			
-			tween2.interpolate_property(targetItem, "position",
-					targetItem.position, draggingItem.position, 0.75,
-					Tween.TRANS_BOUNCE, Tween.EASE_OUT)
-			tween2.start()
-			
+			addTween(draggingItem, targetItem.position)
+			addTween(targetItem, draggingItem.position)
 			
 			draggingItem.isDragging = false
 			draggingItem.endSelection()
@@ -83,13 +93,38 @@ func endDragging(targetItem: RasterItem):
 			draggingItem.endSelection()
 			draggingItem = null
 	
+# add a tween to the moving item, connects to events
+func addTween(item: RasterItem, toPosition: Vector2):
+	var tween = Tween.new()
 	
+	tween.name = "Tween"
+	tween.interpolate_property(item, "position",
+			item.position, toPosition, switchingTime,
+			Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	
+	tween.connect("tween_step", self, "_on_Tween_step", [item])
+	tween.connect("tween_all_completed", self, "_on_Tween_all_completed", [item])
+	
+	item.add_child(tween)
+
+	tween.start()
+	
+	
+# when all movement is done, allow switching again and remove tween
+func _on_Tween_all_completed(item: RasterItem):
+	printt("all movement completed")
+	isSwitching = false
+	item.isSwitching = false
+	item.remove_child(item.get_node("Tween"))
+
+# when movement is done mostly, allow switching again
+func _on_Tween_step(object, key, elapsed, value, item: RasterItem):
+	# allow switching of other items, after 40% of switching time
+	if elapsed > switchingTime * 0.4 and isSwitching:
+		printt("movement at", key, elapsed, value)
+		isSwitching = false
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-
-func _on_Tween1_tween_all_completed():
-	printt("all completed")
-	isSwitching = false
--- godot/RasterItem.gd
+++ godot/RasterItem.gd
@@ -1,47 +1,52 @@
 extends Node2D
 class_name RasterItem
 
+# on mouse down, draggin starts
 signal StartDragging
+# on mouse up, draggin ends
 signal EndDragging
 
+# when item is currently dragged
 var isDragging = false
+# when item is released and currently switching
+# will be reset, when movement is 100% complete
+var isSwitching = false
 
+# column position in the grid
 var rasterX = 0
+# row position in the grid
 var rasterY = 0
+# type of item - currently index in image list
+var itemId = -1
+
 
 const image_list = ["04.png", "08.png", "12.png", "15.png", "16.png"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	var image = image_list[randi() % image_list.size() - 1]
-#	printt('image', image )
+	if(itemId == -1):
+		itemId = randi() % image_list.size() - 1
+		
+	var image = image_list[itemId]
 	var sprite = get_node("Area2D/Sprite")
 	sprite.texture = load('assets/'+ image)
 #	sprite.rotation_degrees = randi() % 360
-	
 	var spritesize = sprite.get_texture().get_size()
-	
-	printt("current size: ", spritesize)
-	
+		
 	var th = 64 * 1.1 #target height
-	var tw = 64 * 1.1#target width
+	var tw = 64 * 1.1 #target width
 	var scale = Vector2( tw / spritesize.x, th / spritesize.y)
 	sprite.scale = scale
-	printt("scale: ", scale)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func startSelection():
 	get_node("Area2D/AnimationPlayer").play("StartDragging")
-	print("should animate")
+#	print("should animate")
 	
 func endSelection():
 	get_node("Area2D/AnimationPlayer").stop(true)
 	get_node("Area2D/Sprite").rotation_degrees = 0
-	print("stop animate")
+#	print("stop animate")
 	
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
